e731d6af8a11837a28aa9c5bf6799e007b6c21e6
rwightman <rick.wightman@q.com>
2015-01-22 14:10:20 -0700

    my update

--- service.js
+++ service.js
@@ -1,12 +1,18 @@
 var app = angular.module('userProfiles');
 
+//---Inject $q into the service
 app.service('mainService', function($http, $q) {
-
-
+//Once injected we can begin using it by adding a deffered variable to handle the $q	
+  var deferred = $q.defer();
   this.getUsers = function() {
-    return $http({
+    $http({
         method: 'GET',
         url: 'http://reqr.es/api/users?page=1'
-    })
+ //Now let's add a promise to our service       
+    }).then(function(response) {
+      deferred.resolve(response.data.data)
+    });
+    // return only successfully processed data does not deal with error problems in the code
+    return deferred.promise;
   }
 });
